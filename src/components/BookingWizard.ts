9e5b6c77fb95de0d95faa8b5f5980de4d0474c11
aydocs <cihanayberk60@gmail.com>
2025-12-25 07:04:37 +0300

    Update ContactPage: Trigger Vercel Build

--- src/components/BookingWizard.ts
+++ src/components/BookingWizard.ts
@@ -273,7 +273,7 @@ export const renderBookingWizard = async (container: HTMLElement) => {
                     locale: Turkish,
                     minDate: "today",
                     disable: [(date) => date.getDay() === 0],
-                    onChange: async (selectedDate, dateStr) => {
+                    onChange: async (selectedDates, dateStr) => {
                         state.date = dateStr;
                         state.time = null;
                         const slotsContainer = container.querySelector('#slots-container');
